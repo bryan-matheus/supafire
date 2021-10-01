02afa717320381b6b16dad3eb7431cf6756b83c6
Bryan <bryan.matheusbmf@gmail.com>
2021-10-01 01:07:07 -0300

    feat: add support to react-hook-form

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,21 +1,39 @@
 import React, { useCallback, useState } from 'react'
+import { useForm } from 'react-hook-form';
+
+type Inputs = {
+  email: string;
+}
 
 function Login() {
+  const {
+    register, 
+    handleSubmit,
+    watch, formState: {errors}
+  } = useForm<Inputs>();
   const [loading, setLoading] = useState(false)
-  const [email, setEmail] = useState('')
   
-  const handleLogin = useCallback(() => {
+  const onSubmit = useCallback(() => {
   }, []);
 
+  console.debug(watch('email'))
+
   return (
-    <main className={"flex justify-center align-center w-screen h-screen"}>
-        <form onSubmit={e => e.preventDefault()}>
-            <section>
-                <label>E-mail</label>
-                <input type={"email"} value={email} onChange={e => setEmail(e.target.value)} />
+    <main className={"flex justify-center align-center w-screen h-screen bg-gray-900 text-white"}>
+        <form onSubmit={handleSubmit(onSubmit)}
+              className={"flex justify-center align-center flex-col w-3/12"}>
+            <section className={"flex justify-center align-center flex-col"}>
+                <label>Your best email</label>
+                <input type={"email"}
+                       placeholder={"jhon@doe.com"}
+                       {...register('email', {required: true})} 
+                       className={"w-full p-4 mt-2 bg-gray-100 rounded text-black"} />
+                {errors.email && <span>{errors.email.message}</span>}
             </section>
-            <button type={"submit"} onClick={handleLogin} disabled={loading}>
-                Entrar na conta
+            <button type={"submit"}
+                    disabled={loading}
+                    className={"w-full p-2 bg-green-500 rounded mt-4 text-white text-lg font-medium hover:bg-green-600"}>
+                Send me the magic link!
             </button>
         </form>
     </main>
